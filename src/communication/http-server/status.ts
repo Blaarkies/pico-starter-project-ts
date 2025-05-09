d73c5443101b08c65128ccda4c832dab0bab13da
Blaarkies <pierre.roux6@gmail.com>
2025-05-09 17:45:05 +0200

    rework server usage

--- src/communication/http-server/status.ts
+++
@@ -1,5 +0,0 @@
-import { coerce } from '../../common/transform';
-
-export function statusIs200s(response: Response): boolean {
-    return coerce(response.status, 200, 299) === response.status;
-}
--- src/custom/server.ts
+++ src/custom/server.ts
@@ -7,15 +7,7 @@ import {
     sequencedInterval,
     waitForDuration,
 } from 'common/time';
-import {
-    toExp,
-    toLog,
-} from 'common/transform';
-import { makeHtmlDocument } from 'communication/http-server/boilerplate';
-import { readRequestBody } from 'communication/http-server/process';
-import { makeHttpServer } from 'communication/http-server/server';
-import { contentTypes } from 'communication/http-server/types';
-import { connectToWifiNetwork } from 'communication/wifi';
+import { toExp } from 'common/transform';
 import {
     animateCenterFloodIn,
     animateCenterFloodOut,
@@ -26,8 +18,9 @@ import {
     animateSweepRight,
 } from 'devices/ws2812/animator/animations/sweep';
 import { PixelAnimator } from 'devices/ws2812/animator/pixel-animator';
+import { MuWebServer } from 'network/http-server/mu-web-server';
+import { connectToWifiNetwork } from 'network/wifi';
 import { IPWM } from 'pwm';
-import { interval } from 'rxjs/internal/observable/interval';
 import { take } from 'rxjs/internal/operators/take';
 import { ColorSelection } from './color-selection';
 
@@ -38,7 +31,7 @@ export async function setupServer(controls: {
     toggleBoardLed: () => void;
     setBoardLed: (isOn: boolean) => void;
 }) {
-    console.log('>< Starting server setup...');
+    console.log('>> Starting server setup...');
     sequencedInterval([100, 300, 200])
         .pipe(take(20))
         .subscribe(() => controls.toggleBoardLed());
@@ -49,8 +42,11 @@ export async function setupServer(controls: {
         '<<SECRET_WIFI_SECURITY>>',
     );
 
-    makeHttpServer({
-        onStartupFn: async (baseUrl: string) => {
+    let server = new MuWebServer({assetsPath: 'assets'})
+        .start(async (error: Error) => {
+            if (error) {
+                return;
+            }
             console.log(`>> Pico controller server is online!`);
             let config = {animationFn: animateCenterFloodOut, duration: 300};
             for (let i = 0; i < 3; i++) {
@@ -64,195 +60,87 @@ export async function setupServer(controls: {
             controls.pixelAnimator.setToColor(controls.colorCycler.selectedRgb,
                 {animationFn: animateCenterFloodIn, duration: 3e3});
             controls.setBoardLed(true);
-        },
+        })
 
-        routes: {
-            '/': async request => {
-                interval(200).pipe(take(4)).subscribe(() => controls.toggleBoardLed());
+        .redirect('/', '/index.html')
 
-                let message = makeHtmlDocument({
-                    title: 'Pico Controller',
-// @formatter:off
-                    body: // language=html
-`<div>Pico Controller</div>
-<button id="pow">Power</button>
-<button id="cybr">Brightness</button>
-<input id="sule" list="sule-ticks" type="range" min="0" max="99" step="1" value="0"/>
-<hr/>
-<label for="anty">Animation</label>
-<select id="anty">
-<option value="fade">Fade</option>
-<option value="sweep-r">Sweep right</option>
-<option value="sweep-l">Sweep left</option>
-<option value="flood-out">Flood out</option>
-<option value="flood-in">Flood in</option>
-<option>Random</option>
-</select>
-<input id="colo" type="color"/>`, // @formatter:on
-                    styleSrc: 'style.css',
-                    scriptSrc: 'script.js',
-                });
+        .get('/api/cycle-brightness', async () => {
+            let cycler = controls.colorCycler;
+            cycler.cyclePower();
+            let newColor = cycler.selectedRgb;
+            controls.pixelAnimator.setToColor(newColor);
 
+            return JSON.stringify(newColor);
+        })
+
+        .get('/api/power-button', async () => {
+            let cycler = controls.colorCycler;
+            let oldColor = cycler.selectedRgb;
+            cycler.toggle();
+            let newColor = cycler.selectedRgb;
+            controls.pixelAnimator.setToColor(
+                newColor,
+                {fromRgb: oldColor, duration: 5e3});
+
+            controls.setBoardLed(sum(newColor) > 0);
+
+            return JSON.stringify(cycler.isPoweredOn);
+        })
+
+        .post('/api/set-super-led', async request => {
+            let newValue = (request as any).body.power;
+
+            if (typeof newValue !== 'number') {
                 return {
-                    message,
-                    contentType: contentTypes.html,
+                    message: JSON.stringify({error: 'Invalid value'}),
+                    status: 400,
                 };
-            },
+            }
 
-            '/style.css': async request => {
+            let normalized = newValue / 100;
+            let curvedValue = toExp(normalized, 20);
+
+            let pwm = controls.brightLedPwm;
+            let safetyFactor = .6;
+            pwm.setDuty(curvedValue * safetyFactor);
+            curvedValue > 0
+            ? pwm.start()
+            : pwm.stop();
+
+            return JSON.stringify(newValue);
+        })
+
+        .post('/api/set-new-color', async request => {
+            let newValue = (request as any).body.power;
+
+            if (typeof newValue.animationType !== 'string'
+                || typeof newValue.toRgb !== 'string') {
                 return {
-                    message: '',
-                    contentType: contentTypes.css,
+                    message: JSON.stringify({error: 'Invalid value'}),
+                    status: 400,
                 };
-            },
+            }
 
-            '/script.js': async request => {
-// @formatter:off
-let message = // language=JavaScript
-`let [cybr, pow, sule, anty, colo] = ['cybr', 'pow', 'sule', 'anty', 'colo']
-    .map(id => document.getElementById(id));
+            let parsedValue = {
+                animationFn: animationMap[newValue.animationType]
+                    ?? pickRandomElement(Object.values(animationMap)),
+                toRgb: hexToRgbColor(newValue.toRgb),
+            };
 
-cybr.addEventListener('click', () => fetch('api/cycle-brightness'));
-pow.addEventListener('click', () => fetch('api/power-button'));
-
-function throttle(fn, delay) {
-    let lastCalled = 0;
-    return (...args) => {
-        let now = new Date().getTime();
-        if(now - lastCalled < delay) {
-            return;
-        }
-        lastCalled = now;
-        return fn(...args);
-    }
-}
-let tdSuperLed = throttle(() => fetch('api/set-super-led', {
-    method: 'POST',
-    headers: {'Content-Type': 'text/json; charset=utf-8'},
-    body: JSON.stringify({power: Number(sule.value)})
-}), 300);
-let tdColorSet = throttle(() => fetch('api/set-new-color', {
-        method: 'POST',
-        headers: {'Content-Type': 'text/json; charset=utf-8'},
-        body: JSON.stringify({animationType: anty.value, toRgb: colo.value})
-}), 300);
-sule.addEventListener('change', tdSuperLed);
-colo.addEventListener('input', tdColorSet);`; // @formatter:on
-
+            if (!parsedValue.animationFn
+                || !Array.isArray(parsedValue.toRgb)) {
                 return {
-                    message,
-                    contentType: contentTypes.javascript,
+                    message: JSON.stringify({error: 'Invalid value'}),
+                    status: 400,
                 };
-            },
+            }
 
-            '/api/cycle-brightness': async request => {
-                let cycler = controls.colorCycler;
-                cycler.cyclePower();
-                let newColor = cycler.selectedRgb;
-                controls.pixelAnimator.setToColor(newColor);
+            controls.pixelAnimator.setToColor(parsedValue.toRgb, {
+                animationFn: parsedValue.animationFn,
+            });
 
-                return {
-                    message: JSON.stringify(newColor),
-                    contentType: contentTypes.json,
-                };
-            },
-
-            '/api/power-button': async request => {
-                let cycler = controls.colorCycler;
-                let oldColor = cycler.selectedRgb;
-                cycler.toggle();
-                let newColor = cycler.selectedRgb;
-                controls.pixelAnimator.setToColor(
-                    newColor,
-                    {
-                        fromRgb: oldColor,
-                        duration: 5e3,
-                    },
-                );
-
-                sum(newColor)
-                ? controls.setBoardLed(true)
-                : controls.setBoardLed(false);
-
-                return {
-                    message: JSON.stringify(cycler.isPoweredOn),
-                    contentType: contentTypes.json,
-                };
-            },
-
-            '/api/set-super-led': async request => {
-                let newValue;
-
-                try {
-                    let body: any = await readRequestBody(request);
-                    newValue = body.power;
-                } catch (e) {
-                    return {status: 400, message: 'Bad input format'};
-                }
-
-                if (typeof newValue !== 'number') {
-                    return {
-                        message: JSON.stringify({error: 'Invalid value'}),
-                        status: 400,
-                    };
-                }
-
-                let normalized = newValue / 100;
-                let curvedValue = toExp(normalized, 20);
-
-                let pwm = controls.brightLedPwm;
-                let safetyFactor = .6;
-                pwm.setDuty(curvedValue * safetyFactor);
-                curvedValue > 0
-                ? pwm.start()
-                : pwm.stop();
-
-                return {message: JSON.stringify(newValue)};
-            },
-
-            '/api/set-new-color': async request => {
-                let newValue;
-
-                try {
-                    let body: any = await readRequestBody(request);
-                    newValue = body;
-                } catch (e) {
-                    return {status: 400, message: 'Bad input format'};
-                }
-
-                if (typeof newValue.animationType !== 'string'
-                    || typeof newValue.toRgb !== 'string') {
-                    return {
-                        message: JSON.stringify({error: 'Invalid value'}),
-                        status: 400,
-                    };
-                }
-
-                let parsedValue = {
-                    animationFn: animationMap[newValue.animationType]
-                        ?? pickRandomElement(Object.values(animationMap)),
-                    toRgb: hexToRgbColor(newValue.toRgb),
-                };
-
-                if (!parsedValue.animationFn
-                    || !Array.isArray(parsedValue.toRgb)) {
-                    return {
-                        message: JSON.stringify({error: 'Invalid value'}),
-                        status: 400,
-                    };
-                }
-
-                controls.pixelAnimator.setToColor(parsedValue.toRgb, {
-                    animationFn: parsedValue.animationFn,
-                });
-
-                return {message: JSON.stringify(newValue.toRgb)};
-            },
-
-        },
-
-    });
-
+            return JSON.stringify(newValue.toRgb);
+        });
 }
 
 let animationMap = {
