Blaarkies/raspberry-pico-js-project-setup

add abort trigger for delayed shutdown

src/main.ts
@@ -1,81 +1,151 @@
+import { Stream } from 'kefir';
 import {
     getHslAtKelvin,
-    MultiActionButton,
-    sum,
+    hslToRgb,
+    sequencedInterval,
+    SequenceIntervalSubject,
     waitForDuration,
 } from './common';
+import { TimeoutHandle } from './common/types';
+import {
+    MultiActionButton,
+    Ws2812,
+} from './devices';
 import { ColorCycler } from './state/color-selection';
 import { PixelsAnimator } from './state/pixels-animator';
-import { Ws2812 } from './ws2812/ws2812';
 
+try {
+    runProgram();
+} catch (error) {
+    console.error('Error in main program\n',
+        error,
+        error.stack
+        );
+}
 
-let ledPin = board.LED;
-pinMode(ledPin, OUTPUT);
+function runProgram() {
+    let ledPin = board.LED;
+    pinMode(ledPin, OUTPUT);
 
-let colorCycler = new ColorCycler(
-    {
-        warm: getHslAtKelvin(2000),
-        cold: getHslAtKelvin(4000),
-        red: [0, 1, .5],
-        green: [.33, 1, .5],
-        blue: [.67, 1, .5],
-    },
-);
+    let colorCycler = new ColorCycler(
+        {
+            warm: getHslAtKelvin(2000),
+            cold: getHslAtKelvin(4000),
+            // red: [0, 1, .5],
+            // green: [.33, 1, .5],
+            // blue: [.67, 1, .5],
+        },
+        [.15, .4, 1],
+    );
 
-let pinButtonA = 0;
-let pinButtonB = 1;
-let pinPixels = 2;
+    let pinButtonA = 0;
+    let pinButtonB = 1;
+    let pinPixels = 2;
 
-let pixels = new Ws2812(pinPixels, 74);
-let pixelsAnimator = new PixelsAnimator(
-    pixels,
-);
-pixelsAnimator.setColor(colorCycler.selectedRgb, {
-    animationType: 'sweep-center-out',
-});
+    let pixels = new Ws2812(pinPixels, 74);
+
+    let pixelsAnimator = new PixelsAnimator(pixels);
+    pixelsAnimator.setColor(colorCycler.selectedRgb, {
+        animationType: 'sweep-center-out',
+    });
 
 
 // Buttons
-let buttonC = new MultiActionButton(pinButtonA);
-let isHeldC: boolean;
-buttonC.onHold({
-    callbackStart: () => isHeldC = true,
-    callbackEnd: () => isHeldC = false,
-});
+    let longPress = {b: false, c: false};
 
-let buttonB = new MultiActionButton(pinButtonB);
-let isHeldB: boolean;
-buttonB.onHold({
-    callbackStart: () => isHeldB = true,
-    callbackEnd: () => isHeldB = false,
-});
+    let buttonC = new MultiActionButton(pinButtonA);
+    buttonC.onLongPress({
+        startFn: () => longPress.c = true,
+        endFn: () => longPress.c = false,
+    });
 
-buttonC.onRelease(async () => {
-    if (isHeldB) {
-        colorCycler.cycleColor();
-        pixelsAnimator.setColor(colorCycler.selectedRgb);
-        return;
-    }
+    let buttonB = new MultiActionButton(pinButtonB);
+    let hueGo = false;
+    buttonB.onLongPress({
+        startFn: () => longPress.b = true,
+        endFn: async () => {
+            console.log('buttonB.onLongPress endFn');
+            longPress.b = false;
 
-    colorCycler.toggle();
+            if (longPress.c) {
+                hueGo = true;
+                let waitMs = 50;
+                let leds = pixels.ledAmount;
+                let run1 = 0;
+                for (let t = 0; hueGo; t++) {
+                    run1 = (run1 + .1) % (leds * 3);
+                    let tNormalized = (t % leds) / leds;
+                    let sine = Math.sin(tNormalized * Math.PI) * 2;
+                    let aRange = [sine * run1, sine * run1 + 10]
+                        .map(n => Math.floor(n) % leds);
 
-    if (sum(colorCycler.selectedRgb) === 0) {
-        let handle = setInterval(() => digitalToggle(ledPin), 500);
-        await waitForDuration(2e3);
-        clearInterval(handle);
+                    let hueColor = hslToRgb(tNormalized, 1, .5);
+                    let hueColor2 = hslToRgb(1 - tNormalized, 1, .5);
+
+                    let aColor = Ws2812.valueFromColor(hueColor2);
+
+                    pixels.fillAllColor(hueColor);
+                    for (let i = aRange[0]; i < aRange[1]; i++) {
+                        pixels.setLed(i, aColor);
+                    }
+
+                    pixels.write();
+                    await waitForDuration(waitMs);
+                }
+            }
+        },
+    });
+
+    let isBusy = false;
+    let shutdownNotification$: SequenceIntervalSubject;
+
+    function abortShutdown() {
+        isBusy = false;
+        shutdownNotification$.stop();
         digitalWrite(ledPin, 0);
     }
 
-    pixelsAnimator.setColor(colorCycler.selectedRgb);
-});
+    buttonC.onRelease(async () => {
+        if (longPress.b) {
+            console.log('c release after longpress b');
+            colorCycler.cycleColor();
+            pixelsAnimator.setColor(colorCycler.selectedRgb);
+            return;
+        }
 
-buttonB.onRelease(() => {
-    colorCycler.cyclePower(isHeldC ? -1 : 1, 3);
-    pixelsAnimator.setColor(colorCycler.selectedRgb);
-});
+        console.log('is busy', isBusy,
+            'isPoweredOn', colorCycler.isPoweredOn);
+        if (isBusy) {
+            abortShutdown();
+            return;
+        }
 
+        if (colorCycler.isPoweredOn) {
+            isBusy = true;
+            shutdownNotification$ = sequencedInterval([100,200,300,400,500])
+                .onValue(() => digitalToggle(ledPin));
+            await waitForDuration(20e3);
 
+            if (!isBusy) {
+                return;
+            }
+            abortShutdown();
+        }
 
+        let oldColor = colorCycler.selectedRgb;
+        colorCycler.toggle();
+        let selectedColor = colorCycler.selectedRgb;
 
+        pixelsAnimator.setColor(selectedColor, {
+            animationType: 'fade',
+            duration: 8e3,
+            fromRgb: oldColor,
+        });
+    });
 
-
+    buttonB.onRelease(() => {
+        hueGo = false;
+        colorCycler.cyclePower(longPress.c ? -1 : 1);
+        pixelsAnimator.setColor(colorCycler.selectedRgb);
+    });
+}

Blaarkies/pico-starter-project-ts

src/main.ts
@@ -1,14 +1,121 @@
-import { PicoCYW43 } from 'pico_cyw43';
-import { interval } from 'rxjs/internal/observable/interval';
+import { IPWM } from 'pwm';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import {
+    getHslAtKelvin,
+    sequencedInterval,
+    waitForDuration,
+} from './common';
+import {
+    MultiActionButton,
+    Ws2812,
+} from './devices';
+import { ColorCycler } from './state/color-selection';
+import { PixelsAnimator } from './state/pixels-animator';
 
-let picoCyw43 = new PicoCYW43();
+try {
+    runProgram();
+} catch (error) {
+    console.error('Error in main program\n',
+        error,
+        error.stack,
+    );
+}
 
-let setLed = (isOn: boolean) => {
-    picoCyw43.putGpio(0, isOn);
-};
-let stateLed = false;
-let toggleLed = () => setLed(stateLed = !stateLed);
+function runProgram() {
+    let ledPin = board.LED;
+    pinMode(ledPin, OUTPUT);
 
-interval(500).subscribe(i => {
-    toggleLed();
-});
+    let colorCycler = new ColorCycler(
+        {
+            warm: getHslAtKelvin(2000),
+            cold: getHslAtKelvin(4000),
+        },
+        [.15, .4, 1],
+    );
+
+    let pinButtonA = 0;
+    let pinButtonB = 1;
+    let pinPixels = 2;
+    let pinExtraLed = 4;
+
+    let extraLed = board.pwm(pinExtraLed, 1500, .3) as IPWM;
+
+    let pixels = new Ws2812(pinPixels, 74);
+
+    let pixelsAnimator = new PixelsAnimator(pixels);
+    pixelsAnimator.setColor(colorCycler.selectedRgb, {
+        animationType: 'sweep-center-out',
+    });
+
+
+// Buttons
+    let longPress = {b: false, c: false};
+
+    let buttonC = new MultiActionButton(pinButtonA);
+    buttonC.onLongPress({
+        startFn: () => longPress.c = true,
+        endFn: () => longPress.c = false,
+    });
+
+    let buttonB = new MultiActionButton(pinButtonB);
+    buttonB.onLongPress({
+        startFn: () => longPress.b = true,
+        endFn: () => longPress.b = false,
+    });
+
+    let isBusy = false;
+    let shutdownNotification$ = new Subject<void>();
+
+    function abortShutdown() {
+        isBusy = false;
+        shutdownNotification$.next();
+        digitalWrite(ledPin, 0);
+    }
+
+    buttonC.onRelease(async () => {
+        if (longPress.b) {
+            colorCycler.cycleColor();
+            pixelsAnimator.setColor(colorCycler.selectedRgb);
+
+            (colorCycler.selected.colorIndex === 1)
+            ? extraLed.start()
+            : extraLed.stop();
+
+            return;
+        }
+
+        if (isBusy) {
+            abortShutdown();
+            return;
+        }
+
+        if (colorCycler.isPoweredOn) {
+            isBusy = true;
+            sequencedInterval([100, 200, 300, 400, 500])
+                .pipe(takeUntil(shutdownNotification$))
+                .subscribe(() => digitalToggle(ledPin));
+            await waitForDuration(20e3);
+
+            if (!isBusy) {
+                return;
+            }
+            abortShutdown();
+        }
+
+        let oldColor = colorCycler.selectedRgb;
+        colorCycler.toggle();
+        let selectedColor = colorCycler.selectedRgb;
+
+        pixelsAnimator.setColor(selectedColor, {
+            animationType: 'fade',
+            duration: 8e3,
+            fromRgb: oldColor,
+        });
+    });
+
+    buttonB.onRelease(() => {
+        colorCycler.cyclePower(longPress.c ? -1 : 1);
+        pixelsAnimator.setColor(colorCycler.selectedRgb);
+    });
+}